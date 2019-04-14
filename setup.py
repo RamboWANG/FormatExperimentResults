7ecf0a978b82f4c67e1d96505e1df6841743c353
wangruibo <wangruibo@sxu.edu.cn>
2019-04-14 10:10:19 +0800

    update package config

---
+++ setup.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="hpcutil",
+    version="0.1",
+    keywords={"hpc", "sxu", "util tools"},
+    description="",
+    long_description="",
+    license="MIT Licence",
+    url = "",
+    author="Wang Ruibo",
+    author_email="wangruibo@sxu.edu.cn",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[],
+    script=[]
+)
